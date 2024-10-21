f81fb5be071f3c82232a894ca539f05ef37486c1
HademiData <adewale.afolabi180221005@st.lasu.edu.ng>
2024-10-21 20:50:51 +0100

    Iterator v1

---
+++ Behavioral Patterns/Iterator.java
@@ -0,0 +1,9 @@
+public class Iterator {
+    
+    // Iterator is a behavioral design pattern that lets you traverse 
+    // elements of a collection without exposing its
+    //  underlying representation (list, stack, tree, etc.).
+
+    
+
+}
